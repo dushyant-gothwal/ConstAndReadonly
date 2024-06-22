1aa77cc1612edbe86439f98a17034e721fefed1d
Dushyant Gothwal <gothwaldushyant@gmail.com>
2024-06-23 02:40:42 +0530

    Add project files.

---
+++ ConstAndReadonly/ConstAndReadonly.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Xsl;
+
+
+namespace ConstAndReadonly
+{
+    public class ConstAndReadonly
+    {
+
+        //constant 
+
+        public const int constVariable = 1;
+
+        private readonly string str;
+
+        public ConstAndReadonly(string a)
+        {
+            str = a;          
+        }
+
+    }
+}
---
+++ ConstAndReadonly/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Xsl;
+
+namespace ConstAndReadonly
+{
+    public class Program
+    {
+
+        public void GetMsg() {
+
+            Console.WriteLine("Hello method is running");
+
+        } 
+
+
+    }
+}
